4c6d0464fba177a564c87b54891d554122346d17
krliman <pogorelovmaxim@gmail.com>
2024-08-05 13:56:30 +0300

    Add last fix

--- src/modules/Products.jsx
+++ src/modules/Products.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
 import { Product } from "./Product";
 import { useProducts } from "../context/ProductContext";
@@ -6,29 +6,34 @@ import { useSearchParams } from "react-router-dom";
 import { SkeletonLoader } from "./SkeletonLoader";
 
 export const Products = () => {
-  const [seachParams] = useSearchParams();
-  const { products, setCategory, categories } = useProducts();
-  const category = seachParams.get("category");
+    const [seachParams] = useSearchParams();
+  
+    const { products, setCategory, categories, productsRef } = useProducts();
+    const category = seachParams.get("category");
 
-  useEffect(() => {
-    setCategory(category);
-  }, [category, setCategory]);
+    useEffect(() => {
+        setCategory(category);
+    }, [category, setCategory]);
 
-  const categoryTitle = categories[category] || "Товары";
 
-  return (
-    <section className="products">
-      <div className="container">
-        <h2 className="products__title">{categoryTitle}</h2>
 
-        <ul className="products__list">
-          {products.length ? (
-            products.map((item) => <Product key={item.id} data={item} />)
-          ) : (
-            <SkeletonLoader />
-          )}
-        </ul>
-      </div>
-    </section>
-  );
+    const categoryTitle = categories[category] || "Товары";
+
+    return (
+        <section className="products" id="#products" ref={productsRef}>
+            <div className="container">
+                <h2 className="products__title">{categoryTitle}</h2>
+
+                <ul className="products__list">
+                    {products.length ? (
+                        products.map((item) => (
+                            <Product key={item.id} data={item} />
+                        ))
+                    ) : (
+                        <SkeletonLoader />
+                    )}
+                </ul>
+            </div>
+        </section>
+    );
 };
